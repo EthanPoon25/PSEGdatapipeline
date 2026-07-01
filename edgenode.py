d60c9298fbf4732b9094a8738725667635c3f8eb
Ethan Poon <ewpoon007@gmail.com>
2026-07-01 14:53:38 -0500

    go server working and validated with postman

--- edgenode.py
+++ edgenode.py
@@ -1,11 +1,27 @@
 import sqlite3
 import json
 import paho.mqtt.client as mqtt
+import requests
 
 brokeradd="localhost"
 port=1883
 topic="trailer_01/sensors/telemetry"
 dbpath="telemetry.db"
+print("Checking database...")
+setup_db = sqlite3.connect(dbpath)
+setup_cursor = setup_db.cursor()
+setup_cursor.execute('''
+    CREATE TABLE IF NOT EXISTS telemetry (
+        unitid TEXT,
+        timestamp TEXT,
+        turbidity REAL,
+        atp REAL,
+        temperature REAL
+    )
+''')
+setup_db.commit()
+setup_db.close()
+print("Database ready!")
 
 def onconnect(client, userdata, flags, rc):
     print("Connected with result code: ",rc)
@@ -14,16 +30,21 @@ def onconnect(client, userdata, flags, rc):
 def onmessage(client, userdata,msg):
     try:
         receiveddata=json.loads(msg.payload.decode('utf-8'))
+        print("here")
         print(f"Received JSON data from `{msg.topic}`: {receiveddata}")
         connecting=sqlite3.connect('telemetry.db')
         cursorobj=connecting.cursor()
         cursorobj.execute("INSERT INTO telemetry (unitid, timestamp,turbidity, atp, temperature) VALUES (?, ?,?,?,?)", (receiveddata["unitid"], receiveddata["timestamp"], receiveddata["turbidity"], receiveddata["atp"], receiveddata["temperature"]))
         connecting.commit()
         connecting.close()
+        response = requests.post("http://localhost:8080/telemetry", json=receiveddata)
+        print("POST status:", response.status_code)
     except json.JSONDecodeError:
         print(f"Received non-JSON message from `{msg.topic}`: {msg.payload.decode()}")
     except KeyError as e:
         print(f"Missing key in JSON data: {e}")
+    except Exception as e:
+        print("Unexpected error:", e)
 
 client = mqtt.Client()
 client.on_connect=onconnect
--- ingestion-api/main.go
+++ ingestion-api/main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
 	"net/http"
 )
 
+type sensorData struct {
+	UnitID      string  `json:"unitid"`
+	Timestamp   string  `json:"timestamp"`
+	Turbidity   float64 `json:"turbidity"`
+	ATP         float64 `json:"atp"`
+	Temperature float64 `json:"temperature"`
+}
+
 func main() {
-	http.HandleFunc("/", handleHello)
+	http.HandleFunc("/health", handleHello)
+	http.HandleFunc("/telemetry", handleTelemetry)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handleHello(w http.ResponseWriter, __ *http.Request) {
-	wc, err := w.Write([]byte("Hello World!\n"))
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	wc, err := w.Write([]byte("Server is running!\n"))
 	if err != nil {
 		slog.Error("error writing response", "err", err)
 		return
 	}
 
-	fmt.Printf("%d bytes written", wc)
+	fmt.Println("%d bytes written", wc)
+}
+
+func handleTelemetry(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		slog.Error("error writing response")
+		return
+	}
+
+	data, err := io.ReadAll((r.Body))
+	if err != nil {
+		slog.Error("error writing response", "err", err, data)
+		return
+	}
+
+	var sensordata sensorData
+	errs := json.Unmarshal(data, &sensordata)
+	if errs != nil {
+		fmt.Println(errs)
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("%+v\n", sensordata)
+	w.WriteHeader(http.StatusOK)
 }
