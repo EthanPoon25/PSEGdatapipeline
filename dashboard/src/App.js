7e786e1671d75163c089084d917f55c82aa98725
Ethan Poon <ewpoon007@gmail.com>
2026-07-08 15:13:10 -0500

    dashboard w/ backend configuration

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [readings, setReadings] = useState([])
+  const [isSpiking, setIsSpiking] = useState(false)
+  useEffect(() => {
+      const interval = setInterval(() => {
+          axios.get("http://localhost:8080/data").then(res => {
+              setReadings(res.data)
+              setIsSpiking(res.data.some(r => r.turbidity > 5.0))
+          })
+      }, 2000)
+      return () => clearInterval(interval)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
