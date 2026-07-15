631ec601607baeb3622066afa8882b214fe67a86
Ethan Poon <ewpoon007@gmail.com>
2026-07-15 15:00:56 -0500

    minioupload func draft

--- archiver.py
+++ archiver.py
@@ -30,3 +30,10 @@ def writecsv(rows):
     return filename
 
 def minioupload(filename):
+    s3_client = boto3.client('s3')
+    with open(filename, 'awms') as f:
+        s3_client.put_object(
+            endpoint_url='http://localhost:9000',
+            aws_access_key_id='minioadmin',
+            aws_secret_access_key='minioadmin'
+        )
